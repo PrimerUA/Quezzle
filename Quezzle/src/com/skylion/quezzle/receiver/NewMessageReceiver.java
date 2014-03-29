ace9bbb3a40345ff6c8b07c5bae10b1541f8c04e
Roman Bielokon <roman_slavyansk@mail.ru>
2014-03-29 21:42:43 +0200

    added receiver

---
+++ Quezzle/src/com/skylion/quezzle/receiver/NewMessageReceiver.java
@@ -0,0 +1,38 @@
+package com.skylion.quezzle.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.util.Log;
+import com.google.gson.Gson;
+import com.skylion.quezzle.service.NetworkService;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kvest
+ * Date: 29.03.14
+ * Time: 20:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewMessageReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Gson gson = new Gson();
+        String data = intent.getExtras().getString("com.parse.Data");
+        if (!TextUtils.isEmpty(data)) {
+            //parse push data
+            PushData pushData = gson.fromJson(data, PushData.class);
+            //start updating chat
+            NetworkService.refreshChat(context, pushData.chatObjectId);
+        } else {
+            Log.e("KVEST_TAG", "NewMessageReceiver error - empty data");
+        }
+    }
+
+    private static class PushData {
+        String action;
+        String chatObjectId;
+    }
+}
