c7fdcd4f3b9e0101114b09eb2b86858efb591a98
PrimerUA <mikeshapiro92@gmail.com>
2014-05-20 00:02:43 +0300

    geo icon + permission

--- Quezzle/src/com/skylion/quezzle/ui/adapter/ChatListAdapter.java
+++ Quezzle/src/com/skylion/quezzle/ui/adapter/ChatListAdapter.java
@@ -8,100 +8,99 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.skylion.quezzle.R;
 import com.skylion.quezzle.datastorage.table.ChatPlaceTable;
 import com.skylion.quezzle.utility.Constants;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Kvest
- * Date: 23.03.14
- * Time: 11:01
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: Kvest Date: 23.03.14 Time: 11:01 To change
+ * this template use File | Settings | File Templates.
  */
 public class ChatListAdapter extends CursorAdapter {
-    public static final String[] PROJECTION = new String[] {ChatPlaceTable._ID, ChatPlaceTable.NAME_COLUMN,
-                                                            ChatPlaceTable.DESCRIPTION_COLUMN, ChatPlaceTable.OBJECT_ID_COLUMN,
-                                                            ChatPlaceTable.CHAT_TYPE_COLUMN};
-    private int nameColumnIndex = -1;
-    private int descriptionColumnIndex = -1;
-    private int objectIdColumnIndex = -1;
-    private int chatTypeColumnIndex = -1;
+	public static final String[] PROJECTION = new String[] { ChatPlaceTable._ID, ChatPlaceTable.NAME_COLUMN,
+			ChatPlaceTable.DESCRIPTION_COLUMN, ChatPlaceTable.OBJECT_ID_COLUMN, ChatPlaceTable.CHAT_TYPE_COLUMN };
+	private int nameColumnIndex = -1;
+	private int descriptionColumnIndex = -1;
+	private int objectIdColumnIndex = -1;
+	private int chatTypeColumnIndex = -1;
 
-    public ChatListAdapter(Context context, int flags) {
-        super(context, null, flags);
-    }
+	public ChatListAdapter(Context context, int flags) {
+		super(context, null, flags);
+	}
 
-    private boolean isColumnIndexesCalculated() {
-        return (nameColumnIndex >= 0);
-    }
+	private boolean isColumnIndexesCalculated() {
+		return (nameColumnIndex >= 0);
+	}
 
-    private void calculateColumnIndexes(Cursor cursor) {
-        nameColumnIndex = cursor.getColumnIndex(ChatPlaceTable.NAME_COLUMN);
-        descriptionColumnIndex = cursor.getColumnIndex(ChatPlaceTable.DESCRIPTION_COLUMN);
-        objectIdColumnIndex = cursor.getColumnIndex(ChatPlaceTable.OBJECT_ID_COLUMN);
-        chatTypeColumnIndex = cursor.getColumnIndex(ChatPlaceTable.CHAT_TYPE_COLUMN);
-    }
+	private void calculateColumnIndexes(Cursor cursor) {
+		nameColumnIndex = cursor.getColumnIndex(ChatPlaceTable.NAME_COLUMN);
+		descriptionColumnIndex = cursor.getColumnIndex(ChatPlaceTable.DESCRIPTION_COLUMN);
+		objectIdColumnIndex = cursor.getColumnIndex(ChatPlaceTable.OBJECT_ID_COLUMN);
+		chatTypeColumnIndex = cursor.getColumnIndex(ChatPlaceTable.CHAT_TYPE_COLUMN);
+	}
 
-    @Override
-    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
-        //create view
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.chat_list_item, viewGroup, false);
+	@Override
+	public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+		// create view
+		LayoutInflater inflater = LayoutInflater.from(context);
+		View view = inflater.inflate(R.layout.chat_list_item, viewGroup, false);
 
-        //create holder
-        ViewHolder holder = new ViewHolder();
-        holder.objectId = "";
-        holder.name = (TextView)view.findViewById(R.id.chat_name);
-        holder.description = (TextView)view.findViewById(R.id.chat_description);
-        view.setTag(holder);
-        
-        Animation animation = AnimationUtils.loadAnimation(context, R.anim.chat_fade_in);
+		// create holder
+		ViewHolder holder = new ViewHolder();
+		holder.objectId = "";
+		holder.name = (TextView) view.findViewById(R.id.chat_name);
+		holder.description = (TextView) view.findViewById(R.id.chat_description);
+		holder.type = (ImageView) view.findViewById(R.id.chat_type);
+		view.setTag(holder);
+
+		Animation animation = AnimationUtils.loadAnimation(context, R.anim.chat_fade_in);
 		animation.setDuration(500);
 		view.startAnimation(animation);
 		animation = null;
 
-        return view;
-    }
+		return view;
+	}
 
-    @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        ViewHolder holder = (ViewHolder)view.getTag();
+	@Override
+	public void bindView(View view, Context context, Cursor cursor) {
+		ViewHolder holder = (ViewHolder) view.getTag();
 
-        if (!isColumnIndexesCalculated()) {
-            calculateColumnIndexes(cursor);
-        }
+		if (!isColumnIndexesCalculated()) {
+			calculateColumnIndexes(cursor);
+		}
 
-        holder.objectId = cursor.getString(objectIdColumnIndex);
-        holder.name.setText(cursor.getString(nameColumnIndex));
-        holder.description.setText(cursor.getString(descriptionColumnIndex));
-        switch (cursor.getInt(chatTypeColumnIndex)) {
-            case Constants.ChatType.USUAL :
-                //TODO mark chat as usual
-                break;
-            case Constants.ChatType.GEO :
-                //TODO mark chat as geo
-                break;
-        }
-    }
+		holder.objectId = cursor.getString(objectIdColumnIndex);
+		holder.name.setText(cursor.getString(nameColumnIndex));
+		holder.description.setText(cursor.getString(descriptionColumnIndex));
+		switch (cursor.getInt(chatTypeColumnIndex)) {
+		case Constants.ChatType.USUAL:
+			holder.type.setImageResource(R.drawable.point_main);
+			break;
+		case Constants.ChatType.GEO:
+			holder.type.setImageResource(android.R.drawable.ic_menu_compass);
+			break;
+		}
+	}
 
-    public String getChatKey(View view, int position, long id) {
-        Object tag = view.getTag();
-        if (tag == null) {
-            return "";
-        }
+	public String getChatKey(View view, int position, long id) {
+		Object tag = view.getTag();
+		if (tag == null) {
+			return "";
+		}
 
-        try {
-            return ((ViewHolder)tag).objectId;
-        } catch (ClassCastException cce) {
-            return "";
-        }
-    }
+		try {
+			return ((ViewHolder) tag).objectId;
+		} catch (ClassCastException cce) {
+			return "";
+		}
+	}
 
-    private static class ViewHolder {
-        public String objectId;
-        public TextView name;
-        public TextView description;
-    }
+	private static class ViewHolder {
+		public String objectId;
+		public TextView name;
+		public TextView description;
+		public ImageView type;
+	}
 }
